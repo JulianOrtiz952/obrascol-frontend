a34716251d4e95888053ff6a3a5056402d8b0821
JulianOrtiz972 <ortizjaimesandresjulian120@gmail.com>
2026-02-17 09:15:37 -0500

    Aumento timeout

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import { Bodega, Subbodega, BodegaStock, PaginatedResponse, Marca, Material, Uni
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/',
-    timeout: 10000,
+    timeout: 60000,
 });
 
 // Interceptor para agregar el token a las solicitudes
